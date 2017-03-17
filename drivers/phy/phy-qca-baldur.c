caf-backup/quic_la_kernel_msm

ipq40xx: usb: fix usb2 disconnect detect issue
turn on host disconnect fix bit only during port reset

Change-Id: I411b3e5d10948a45796752c924966a6f674877ae
Signed-off-by: Senthilkumar N L <snlakshm@codeaurora.org>

drivers/usb/phy/phy-qca-baldur.c
@@ -98,6 +98,20 @@ static inline void qca_baldur_hs_write(void __iomem *base, u32 offset, u32 val)
 }
 
 /**
+ * Read register.
+ *
+ * @base - PHY base virtual address.
+ * @offset - register offset.
+ */
+static inline u32 qca_baldur_hs_read(void __iomem *base, u32 offset)
+{
+	u32 val;
+
+	val = readl(base + offset);
+	return val;
+}
+
+/**
  * Write register and read back masked value to confirm it is written
  *
  * @base - PHY base virtual address.
@@ -121,6 +135,21 @@ static inline void qca_baldur_hs_write_readback(void __iomem *base, u32 offset,
 
 	if (tmp != val)
 		pr_err("write: %x to BALDUR PHY: %x FAILED\n", val, offset);
+}
+
+static int qca_baldur_phy_read(struct usb_phy *x, u32 reg)
+{
+	struct qca_baldur_hs_phy *phy = phy_to_dw_phy(x);
+
+	return qca_baldur_hs_read(phy->base, reg);
+}
+
+static int qca_baldur_phy_write(struct usb_phy *x, u32 val, u32 reg)
+{
+	struct qca_baldur_hs_phy *phy = phy_to_dw_phy(x);
+
+	qca_baldur_hs_write(phy->base, reg, val);
+	return 0;
 }
 
 static int qca_baldur_hs_phy_init(struct usb_phy *x)
@@ -217,6 +246,11 @@ static void qca_baldur_hs_phy_shutdown(struct usb_phy *x)
 	qca_baldur_hs_put_resources(phy);
 }
 
+static struct usb_phy_io_ops qca_baldur_io_ops = {
+	.read = qca_baldur_phy_read,
+	.write = qca_baldur_phy_write,
+};
+
 static const struct qca_baldur_hs_data usb3_hs_data = {
 	.usb3_hs_phy = 1,
 	.phy_config_offset = USB30_HS_PHY_HOST_MODE,
@@ -259,11 +293,12 @@ static int qca_baldur_hs_probe(struct platform_device *pdev)
 		return err;
 	}
 
-	phy->phy.dev        = phy->dev;
-	phy->phy.label      = "qca-baldur-hsphy";
-	phy->phy.init       = qca_baldur_hs_phy_init;
-	phy->phy.shutdown   = qca_baldur_hs_phy_shutdown;
-	phy->phy.type       = USB_PHY_TYPE_USB2;
+	phy->phy.dev		= phy->dev;
+	phy->phy.label		= "qca-baldur-hsphy";
+	phy->phy.init		= qca_baldur_hs_phy_init;
+	phy->phy.shutdown	= qca_baldur_hs_phy_shutdown;
+	phy->phy.type		= USB_PHY_TYPE_USB2;
+	phy->phy.io_ops		= &qca_baldur_io_ops;
 
 	err = usb_add_phy_dev(&phy->phy);
 	return err;

drivers/usb/dwc3/core.c
@@ -72,6 +72,25 @@ void dwc3_usb2phy_suspend(void *priv,
 	}
 }
 
+void dwc3_usb2phy_host_discon(void *priv, bool enable)
+{
+	struct dwc3 *dwc = (struct dwc3 *) priv;
+	u32 reg;
+
+	if (!dwc->enable_usb2_host_discon_quirk)
+		return;
+
+	if (enable) {
+		reg = usb_phy_io_read(dwc->usb2_phy, dwc->phy_misc_reg);
+		reg |= dwc->phy_host_disc_on;
+		usb_phy_io_write(dwc->usb2_phy, reg, dwc->phy_misc_reg);
+	} else {
+		reg = usb_phy_io_read(dwc->usb2_phy, dwc->phy_misc_reg);
+		reg &= ~dwc->phy_host_disc_on;
+		usb_phy_io_write(dwc->usb2_phy, reg, dwc->phy_misc_reg);
+	}
+}
+
 void dwc3_set_mode(struct dwc3 *dwc, u32 mode)
 {
 	u32 reg;
@@ -440,6 +459,20 @@ static int dwc3_probe(struct platform_device *pdev)
 		dwc->needs_fifo_resize = of_property_read_bool(node, "tx-fifo-resize");
 		dwc->enable_usb2susphy_quirk = of_property_read_bool(node,
 							"usb2-susphy-quirk");
+		dwc->enable_usb2_host_discon_quirk =
+			of_property_read_bool(node, "usb2-host-discon-quirk");
+
+		if ((dwc->enable_usb2_host_discon_quirk) &&
+			(of_property_read_u32(node,
+					"usb2-host-discon-phy-misc-reg",
+					&dwc->phy_misc_reg) ||
+			of_property_read_u32(node, "usb2-host-discon-mask",
+					&dwc->phy_host_disc_on)))
+
+		{
+			dev_err(dev, "missing discon quirk params\n");
+			return -ENODEV;
+		}
 
 		dwc->dr_mode = of_usb_get_dr_mode(node);
 	} else if (pdata) {
@@ -458,6 +491,11 @@ static int dwc3_probe(struct platform_device *pdev)
 	if (dwc->enable_usb2susphy_quirk) {
 		dwc->susphy.priv = (void *)dwc;
 		dwc->susphy.set_suspend = &dwc3_usb2phy_suspend;
+	}
+
+	if (dwc->enable_usb2susphy_quirk) {
+		dwc->susphy.priv = (void *)dwc;
+		dwc->susphy.set_host_discon = &dwc3_usb2phy_host_discon;
 	}
 
 	/* default to superspeed if no maximum_speed passed */

sricharanaz/kernel

(cherry picked from commit 2fcf044ab9607e20a57b0407bbbd5d529c79febc)

drivers/phy/phy-qca-baldur.c
@@ -88,6 +88,21 @@ static inline u32 qca_baldur_hs_read(void __iomem *base, u32 offset)
 	return val;
 }
 
+static int qca_baldur_phy_read(struct phy *x, u32 reg)
+{
+	struct qca_baldur_hs_phy *phy = phy_get_drvdata(x);
+
+	return qca_baldur_hs_read(phy->base, reg);
+}
+
+static int qca_baldur_phy_write(struct phy *x, u32 val, u32 reg)
+{
+	struct qca_baldur_hs_phy *phy = phy_get_drvdata(x);
+
+	qca_baldur_hs_write(phy->base, reg, val);
+	return 0;
+}
+
 static int qca_baldur_hs_phy_init(struct phy *x)
 {
 	struct qca_baldur_hs_phy	*phy = phy_get_drvdata(x);
@@ -176,6 +191,8 @@ MODULE_DEVICE_TABLE(of, qca_baldur_hs_id_table);
 static const struct phy_ops ops = {
 	.init           = qca_baldur_hs_phy_init,
 	.exit           = qca_baldur_hs_phy_shutdown,
+	.read		= qca_baldur_phy_read,
+	.write		= qca_baldur_phy_write,
 	.owner          = THIS_MODULE,
 };
 

drivers/usb/dwc3/core.c
@@ -75,6 +75,25 @@ void dwc3_usb2phy_suspend(void *priv,
 	}
 }
 
+void dwc3_usb2phy_host_discon(void *priv, bool enable)
+{
+	struct dwc3 *dwc = (struct dwc3 *) priv;
+	u32 reg;
+
+	if (!dwc->enable_usb2_host_discon_quirk)
+		return;
+
+	if (enable) {
+		reg = phy_io_read(dwc->usb2_generic_phy, dwc->phy_misc_reg);
+		reg |= dwc->phy_host_disc_on;
+		phy_io_write(dwc->usb2_generic_phy, reg, dwc->phy_misc_reg);
+	} else {
+		reg = phy_io_read(dwc->usb2_generic_phy, dwc->phy_misc_reg);
+		reg &= ~dwc->phy_host_disc_on;
+		phy_io_write(dwc->usb2_generic_phy, reg, dwc->phy_misc_reg);
+	}
+}
+
 void dwc3_set_mode(struct dwc3 *dwc, u32 mode)
 {
 	u32 reg;
@@ -931,6 +950,18 @@ static int dwc3_probe(struct platform_device *pdev)
 				"snps,usb3_lpm_capable");
 	dwc->enable_usb2susphy_quirk = device_property_read_bool(dev,
 				"usb2-susphy-quirk");
+	dwc->enable_usb2_host_discon_quirk =
+			device_property_read_bool(dev, "usb2-host-discon-quirk");
+
+	if ((dwc->enable_usb2_host_discon_quirk) &&
+		(device_property_read_u32(dev,
+				"usb2-host-discon-phy-misc-reg",
+				&dwc->phy_misc_reg) ||
+			device_property_read_u32(dev, "usb2-host-discon-mask",
+					&dwc->phy_host_disc_on))) {
+		dev_err(dev, "missing discon quirk params\n");
+		return -ENODEV;
+	}
 
 	dwc->needs_fifo_resize = device_property_read_bool(dev,
 				"tx-fifo-resize");
@@ -1005,6 +1036,11 @@ static int dwc3_probe(struct platform_device *pdev)
 	if (dwc->enable_usb2susphy_quirk) {
 		dwc->susphy.priv = (void *)dwc;
 		dwc->susphy.set_suspend = &dwc3_usb2phy_suspend;
+	}
+
+	if (dwc->enable_usb2_host_discon_quirk) {
+		dwc->susphy.priv = (void *)dwc;
+		dwc->susphy.set_host_discon = &dwc3_usb2phy_host_discon;
 	}
 
 	/* default to superspeed if no maximum_speed passed */