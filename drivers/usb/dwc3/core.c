caf-backup/quic_la_kernel_msm

ipq806x: usb: Reset war for SS device enum issue
Reset again so that if SS device was detected as HS, the
second reset would push it to SS.

Change-Id: I4c76daf8ebeb2d7098d4953af4e01c9ba56342ae
Signed-off-by: Nitheesh Sekar <nsekar@codeaurora.org>

drivers/usb/dwc3/core.c
@@ -962,6 +962,8 @@ static int dwc3_probe(struct platform_device *pdev)
 				"snps,usb3_lpm_capable");
 	dwc->enable_usb2susphy_quirk = device_property_read_bool(dev,
 				"usb2-susphy-quirk");
+	dwc->usb3_dev_reset_quirk = of_property_read_bool(dev,
+				"usb3_dev_reset_quirk");
 	dwc->enable_usb2_host_discon_quirk =
 			device_property_read_bool(dev, "usb2-host-discon-quirk");
 

sricharanaz/kernel

Signed-off-by: Senthilkumar N L <snlakshm@codeaurora.org>

drivers/usb/dwc3/core.c
@@ -962,6 +962,8 @@ static int dwc3_probe(struct platform_device *pdev)
 				"snps,usb3_lpm_capable");
 	dwc->enable_usb2susphy_quirk = device_property_read_bool(dev,
 				"usb2-susphy-quirk");
+	dwc->usb3_dev_reset_quirk = device_property_read_bool(dev,
+				"usb3_dev_reset_quirk");
 	dwc->enable_usb2_host_discon_quirk =
 			device_property_read_bool(dev, "usb2-host-discon-quirk");
 