0265a48f95461e55fa70922d1ab9ffb90a86bce1
esong <song@codeaurora.org>
2017-07-04 11:05:29 +0800

    phy: support aq109 phy
    support aq109 phy
    
    Change-Id: Ia3f7be3bf450aacbad97bd981d5baa8af2eaef70
    Signed-off-by: esong <song@codeaurora.org>

--- drivers/net/phy/aquantia.c
+++ drivers/net/phy/aquantia.c
@@ -24,6 +24,7 @@
 #define PHY_ID_AQR105	0x03a1b4a2
 #define PHY_ID_AQR405	0x03a1b4b0
 #define PHY_ID_AQR107	0x03a1b4e2
+#define PHY_ID_AQR109	0x03a1b502
 
 #define AQ_PHY_MAX_VALID_MMD_REG		0xff01
 #define AQ_PHY_MAX_INVALID_MMD_REG		0xffff
@@ -540,6 +541,23 @@ static struct phy_driver aquantia_driver[] = {
 	.match_phy_device	= aquantia_match_phy_device,
 	.driver		= { .owner = THIS_MODULE,},
 },
+{
+	.phy_id		= PHY_ID_AQR109,
+	.phy_id_mask	= 0xfffffff0,
+	.name		= "Aquantia AQR109",
+	.features	= PHY_AQUANTIA_FEATURES,
+	.flags		= PHY_HAS_INTERRUPT,
+	.probe		= aquantia_phy_probe,
+	.soft_reset	= aquantia_soft_reset,
+	.config_init	= aquantia_config_init,
+	.aneg_done	= aquantia_aneg_done,
+	.config_aneg	= aquantia_config_aneg,
+	.config_intr	= aquantia_config_intr,
+	.ack_interrupt	= aquantia_ack_interrupt,
+	.read_status	= aquantia_read_status,
+	.update_link	= aquantia_update_link,
+	.driver		= { .owner = THIS_MODULE,},
+},
 };
 
 module_phy_driver(aquantia_driver);
@@ -550,6 +568,7 @@ static struct mdio_device_id __maybe_unused aquantia_tbl[] = {
 	{ PHY_ID_AQR105, 0xfffffff0 },
 	{ PHY_ID_AQR405, 0xfffffff0 },
 	{ PHY_ID_AQR107, 0xfffffff0 },
+	{ PHY_ID_AQR109, 0xfffffff0 },
 	{ }
 };
 
