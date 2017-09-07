ac196517cabac0be6276249279c0f850cd38b5f2
Manoharan Vijaya Raghavan <mraghava@codeaurora.org>
2017-09-07 19:26:19 +0530

    ipq807x: rproc: Rproc shutdown for scm call is fixed
    RPROC shutdown call for scm was using old format. This
    is not compatible with new 64 bit TZ, hence fixed it.
    
    Change-Id: If34da1ba267f602aa98188db642bc54d5120f582
    Signed-off-by: Manoharan Vijaya Raghavan <mraghava@codeaurora.org>

--- drivers/firmware/qcom_scm-32.c
+++ drivers/firmware/qcom_scm-32.c
@@ -725,13 +725,21 @@ int __qcom_scm_pas_shutdown(struct device *dev, u32 peripheral)
 	__le32 out;
 	__le32 in;
 	int ret;
+	struct scm_desc desc = {0};
 
-	in = cpu_to_le32(peripheral);
-	ret = qcom_scm_call(dev, QCOM_SCM_SVC_PIL,
+	if (!is_scm_armv8()) {
+		in = cpu_to_le32(peripheral);
+		ret = qcom_scm_call(dev, QCOM_SCM_SVC_PIL,
 			    QCOM_SCM_PAS_SHUTDOWN_CMD,
 			    &in, sizeof(in),
 			    &out, sizeof(out));
-
+	} else {
+		desc.args[0] = peripheral;
+		desc.arginfo = SCM_ARGS(1);
+		ret = qcom_scm_call2(SCM_SIP_FNID(QCOM_SCM_SVC_PIL,
+				QCOM_SCM_PAS_SHUTDOWN_CMD), &desc);
+		out = desc.ret[0];
+	}
 	return ret ? : le32_to_cpu(out);
 }
 
