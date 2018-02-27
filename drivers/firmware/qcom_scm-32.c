083b057b4f31d2b21fbe3918d793a05477613cc1
Pavithra Palanisamy <pavip@codeaurora.org>
2018-02-27 13:22:57 +0530

    ipq807x: Modify qcom_scm_is_call_available() to support armv8.
    Change-Id: I5492b40d4c5e2d20b9de5ac4f1efd6653ab1d038
    Signed-off-by: Pavithra Palanisamy <pavip@codeaurora.org>

--- drivers/firmware/qcom_scm-32.c
+++ drivers/firmware/qcom_scm-32.c
@@ -580,16 +580,32 @@ int __qcom_scm_tls_hardening(struct device *dev,
 int __qcom_scm_is_call_available(struct device *dev, u32 svc_id, u32 cmd_id)
 {
 	int ret;
-	__le32 svc_cmd = cpu_to_le32((svc_id << 10) | cmd_id);
-	__le32 ret_val = 0;
 
-	ret = qcom_scm_call(dev, QCOM_SCM_SVC_INFO, QCOM_IS_CALL_AVAIL_CMD,
-			    &svc_cmd, sizeof(svc_cmd), &ret_val,
-			    sizeof(ret_val));
-	if (ret)
-		return ret;
+	if (!is_scm_armv8()) {
+		__le32 svc_cmd = cpu_to_le32((svc_id << 10) | cmd_id);
+		__le32 ret_val = 0;
 
-	return le32_to_cpu(ret_val);
+		ret = qcom_scm_call(dev, QCOM_SCM_SVC_INFO,
+				QCOM_IS_CALL_AVAIL_CMD, &svc_cmd,
+				sizeof(svc_cmd), &ret_val, sizeof(ret_val));
+
+		if (!ret)
+			return le32_to_cpu(ret_val);
+	} else {
+		__le32 scm_ret;
+		struct scm_desc desc = {0};
+
+		desc.args[0] = SCM_SIP_FNID(svc_id, cmd_id);
+		desc.arginfo = SCM_ARGS(1);
+		ret = qcom_scm_call2(SCM_SIP_FNID(QCOM_SCM_SVC_INFO,
+					QCOM_IS_CALL_AVAIL_CMD), &desc);
+		scm_ret = desc.ret[0];
+
+		if (!ret)
+			return le32_to_cpu(scm_ret);
+	}
+
+	return ret;
 }
 
 int __qcom_qfprom_show_authenticate(struct device *dev, char *buf)
