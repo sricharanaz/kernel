170a7177cc568218e46752b3b14da141858ea9b1
speriaka <speriaka@codeaurora.org>
2018-05-08 19:42:27 +0530

    qcom: ipq: Removed third party markings
    This patch involves changes which reverts
    the third party copyright years update
    
    Change-Id: Id0f28cac6b442f8f3ec3f0c251890593124ac5c5
    Signed-off-by: speriaka <speriaka@codeaurora.org>

--- drivers/firmware/qcom_scm-32.c
+++ drivers/firmware/qcom_scm-32.c
@@ -1,5 +1,5 @@
 /* Copyright (c) 2010, 2015-2018, The Linux Foundation. All rights reserved.
- * Copyright (C) 2015-2018, Linaro Ltd.
+ * Copyright (C) 2015 Linaro Ltd.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
